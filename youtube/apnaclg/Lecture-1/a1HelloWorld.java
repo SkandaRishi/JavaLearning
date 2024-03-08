2fdb4dc9f7047369d1eba2ae82586471f83c0170
SkandaRishi <1247ankit@gmail.com>
2024-03-09 00:04:23 +0530

    Hello World!

---
+++ youtube/apnaclg/Lecture-1/a1HelloWorld.java
@@ -0,0 +1,8 @@
+// Define the class HelloWorld
+public class a1HelloWorld {
+    // Define the main method, the entry point of the program
+    public static void main(String args[]) {
+        // Print "Hello World!" to the console
+        System.out.println("Hello World!");
+    }
+}
