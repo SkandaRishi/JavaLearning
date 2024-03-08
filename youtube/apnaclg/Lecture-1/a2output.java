1a545ae559616731b423901f1edae75a4d15452e
SkandaRishi <1247ankit@gmail.com>
2024-03-09 00:05:50 +0530

    Method Of Output

---
+++ youtube/apnaclg/Lecture-1/a2output.java
@@ -0,0 +1,17 @@
+// Define the class a2output
+public class a2output {
+    // Define the main method, the entry point of the program
+    public static void main(String[] args) {
+        // Print "Hello World with java" without a new line
+        System.out.print("Hello World with java");
+
+        // Print "Hello World with java" with a new line
+        System.out.println("Hello World with java");
+
+        // Use \n to print "Hello World with java" with a new line
+        System.out.print("Hello World with java\n");
+
+        // Print "Hello World with java" without a new line
+        System.out.print("Hello World with java");
+    }
+}
