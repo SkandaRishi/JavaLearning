672dda6df6384353947a01867911eb41e96433aa
SkandaRishi <1247ankit@gmail.com>
2024-03-09 00:06:48 +0530

    Print the pattern

---
+++ youtube/apnaclg/Lecture-1/Task1.java
@@ -0,0 +1,14 @@
+/* Print the pattern
+ * "*
+ *  **
+ *  ***
+ *  ****"
+ */
+public class Task1 {
+    public static void main(String[] args){
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+    }
+}
