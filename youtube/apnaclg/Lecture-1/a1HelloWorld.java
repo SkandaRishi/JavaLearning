47986eda60c1b209f2d1e216016e1f89e06e9865
SkandaRishi <1247ankit@gmail.com>
2024-03-09 09:15:50 +0530

    ✨ feat: Add HelloWorld Java program

--- youtube/apnaclg/Lecture-1/a1HelloWorld.java
+++ youtube/apnaclg/Lecture-1/a1HelloWorld.java
@@ -1,4 +1,5 @@
 // Define the class HelloWorld
+//Always take class name and file name same
 public class a1HelloWorld {
     // Define the main method, the entry point of the program
     public static void main(String args[]) {
