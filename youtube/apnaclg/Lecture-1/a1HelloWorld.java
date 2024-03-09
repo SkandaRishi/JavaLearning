e3a36a7ff024923f12eb1806feb36f85df1d35da
SkandaRishi <1247ankit@gmail.com>
2024-03-09 09:40:59 +0530

    want to restart

--- youtube/apnaclg/Lecture-1/a1HelloWorld.java
+++
@@ -1,9 +0,0 @@
-// Define the class HelloWorld
-//Always take class name and file name same
-public class a1HelloWorld {
-    // Define the main method, the entry point of the program
-    public static void main(String args[]) {
-        // Print "Hello World!" to the console
-        System.out.println("Hello World!");
-    }
-}
