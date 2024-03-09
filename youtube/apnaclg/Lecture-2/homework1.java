79131ccc7687843e0ab302fa77a9cac363386650
SkandaRishi <1247ankit@gmail.com>
2024-03-09 20:07:26 +0530

    🌟 Define meaningful variables: Initialize variables of appropriate types in Java program 🚀

---
+++ youtube/apnaclg/Lecture-2/homework1.java
@@ -0,0 +1,20 @@
+/* Try to declare meaningful variables of each type. Eg - a variable named age should be a numeric type (int or float) not byte. */
+public class homework1 {
+    public static void main(String[] args) {
+    // Declare and initialize variables of appropriate types
+    int age = 25; // Age should typically be stored as an integer
+    float weight = 65.5f; // Weight can be stored as a floating-point number
+    double height = 172.5; // Height can also be stored as a floating-point number
+    char gender = 'M'; // Gender can be represented using a single character
+    boolean isStudent = true; // isStudent can be represented as a boolean value
+
+    // Print out the values of the variables
+    System.out.println("Age: " + age);
+    System.out.println("Weight: " + weight);
+    System.out.println("Height: " + height);
+    System.out.println("Gender: " + gender);
+    System.out.println("Is Student: " + isStudent);
+}
+}
+    
+
