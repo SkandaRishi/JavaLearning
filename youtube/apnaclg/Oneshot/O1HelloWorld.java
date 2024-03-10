9df8536751d865cc673e6b11cdf9bae1223c75dc
SkandaRishi <1247ankit@gmail.com>
2024-03-10 11:23:36 +0530

    🚀 Begin Java journey: Print -> Hello World! Welcome me to Java 🌟

---
+++ youtube/apnaclg/Oneshot/O1HelloWorld.java
@@ -0,0 +1,8 @@
+// Define a public class named HelloWorld
+public class O1HelloWorld {
+    // Define a main method, which is the entry point of the program
+    public static void main(String args[]) {
+        // Print "Hello World! Welcome me to Java" to the console
+        System.out.println("Hello World! Welcome me to Java");
+    }
+}
