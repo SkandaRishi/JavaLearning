045de9c06bfaa10b67828a0e4abe4208b444c243
SkandaRishi <1247ankit@gmail.com>
2024-03-10 11:39:18 +0530

    🚀 Output exploration: Display message using println in Java program 🌟

---
+++ youtube/apnaclg/Oneshot/O2Output.java
@@ -0,0 +1,7 @@
+public class O2Output {
+    public static void main(String[] args) {
+        //we use "System.out.println()" for displaying the output
+        //Also we can use "sout" as shortcut to write that syntax
+        System.out.println("This is output");
+    }
+}
