0ee51d02c974c6aad04860f1d0be3c92fbde0337
SkandaRishi <1247ankit@gmail.com>
2024-03-10 11:40:57 +0530

    just Remove this file

--- O2Output.java
+++
@@ -1,7 +0,0 @@
-public class O2Output {
-    public static void main(String[] args) {
-        //we use "System.out.println()" for displaying the output
-        //Also we can use "sout" as shortcut to write that syntax
-        System.out.println("This is output");
-    }
-}
