0fddb0e4f92d6e81ec1b51106622fcfda8efebcf
SkandaRishi <1247ankit@gmail.com>
2024-03-12 05:49:48 +0530

    🚀 Initialize variables: Define and assign values to 'name', 'age', and 'me' variables in Java program 🌟

---
+++ youtube/apnaclg/Oneshot/O3Variable.java
@@ -0,0 +1,14 @@
+// Define a public class named O3Variable
+public class O3Variable {
+  
+  // Define a main method, which is the entry point of the program
+  public static void main(String args[]) {
+    // variables
+    String name = "Rishi"; // Define a String variable 'name' and assign it the value "Rishi"
+    int age = 20; // Define an integer variable 'age' and assign it the value 20
+    String me = name; // Define a String variable 'me' and assign it the value of the 'name' variable
+    // Print the concatenated string containing the values of 'name', 'age', and 'me' variables
+    System.out.println(
+        "My name is " + name + ". My age is " + age + ".That enough to me means to know " + me);
+  }
+}
