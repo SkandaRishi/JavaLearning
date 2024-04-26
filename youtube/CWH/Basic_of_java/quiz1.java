a6e73ab995b0ed087ad043030764b7dc39e454f8
SkandaRishi <1247ankit@gmail.com>
2024-04-26 18:20:26 +0530

    🚀 Implemented a Java program to add three numbers 🌟

---
+++ youtube/CWH/Basic_of_java/quiz1.java
@@ -0,0 +1,14 @@
+package youtube.CWH.Basic_of_java;
+
+public class quiz1 {
+    public static void main(String[]args){
+        //write a program to add three number
+        int num1 = 6;
+        int num2 = 4;
+        int num3 = 10;
+        int sum = num1 + num2 + num3;
+        System.out.println("Addition of three number is " + sum);
+
+    }
+    
+}
