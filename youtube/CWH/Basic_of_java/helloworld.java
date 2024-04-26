18fe7831f59f049bd2dec3217b62bfaa60d31227
SkandaRishi <1247ankit@gmail.com>
2024-04-26 17:49:43 +0530

    🚀 Implemented Hello World! program in Java 🌟

---
+++ youtube/CWH/Basic_of_java/helloworld.java
@@ -0,0 +1,12 @@
+package youtube.CWH.Basic_of_java;
+
+public class helloworld {
+    public static void main(String[] args) {
+        // write your code
+        System.out.println("Hello World!");
+        // naming convention
+        // AddTwoNumber --> pascal naming convention use for classes
+        // addTwoNumber --> case naming connvention use for functions
+    }
+
+}
